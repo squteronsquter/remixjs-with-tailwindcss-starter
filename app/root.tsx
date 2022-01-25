549bcab9c1e5b48808bb97e83548846759d16010
Digital Mind <jremesz@gmail.com>
2022-01-25 17:44:15 +0100

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import {
+  Links,
+  LinksFunction,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import type { MetaFunction } from "remix";
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => {
+  return [{rel: 'stylesheet', href: styles }];
+}
+
+export const meta: MetaFunction = () => {
+  return { title: "Studio Ghibli", description: 'This is the description for the Studio Ghibli website' };
+
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-gray-900">
+      <h1 className="text-center font-black text-4xl text-gray-50">Studio Ghibli</h1>
+    </div>
+  );
+}
